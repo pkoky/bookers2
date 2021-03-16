479e7f11e31cc8c4d46223fc12836839711ef594
pkoky <sscsk2224@gmail.com>
2021-03-16 08:47:42 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,9 +16,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    redirect_to books_path
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to books_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
+    @book = Book.find(params[:id])
   end
 
-end 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+
+end
