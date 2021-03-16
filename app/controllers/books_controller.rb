f4e418012db925c57d1ac0f831d9981146ac39ac
pkoky <sscsk2224@gmail.com>
2021-03-16 02:44:20 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,30 @@
 class BooksController < ApplicationController
-  
-  def new
-  end 
-  
-  def create
-  end 
-  
+
   def index
-  end 
-  
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
   def show
-  end 
-  
+  end
+
   def destroy
-  end 
-  
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+
 end
